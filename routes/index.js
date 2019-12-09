d6c0d8d54db83d83b614ef0d11ace5f299ed8c42
Eason Lin <uu9924079@gmail.com>
2019-12-09 15:20:54 +0800

    add cart/product routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const productController = require('../controllers/productController.js')
+const cartController = require('../controllers/cartController.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/products', productController.getProducts)
+
+router.get('/cart', cartController.getCart)
+
+module.exports = router
